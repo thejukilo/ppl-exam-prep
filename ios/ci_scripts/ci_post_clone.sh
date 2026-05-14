909c182425a0cd8051e1eff9eedb979a24bb1ba6
Lode Van de elde <lode@jukilo.com>
2026-05-14 13:44:13 -0700

    Fix shebang and add Node install in ci_post_clone.sh

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,9 +1,16 @@
-bash#!/bin/sh
+#!/bin/sh
 
 set -e
 
+echo "=== Installing Node via Homebrew ==="
+brew install node
+
+echo "=== Installing npm dependencies ==="
+cd "$CI_PRIMARY_REPOSITORY_PATH"
+npm install --legacy-peer-deps
+
 echo "=== Installing CocoaPods dependencies ==="
-# Script is in ios/ci_scripts/, parent is ios/ which is where we want to be
-cd "$(dirname "$0")/.."
+cd "$CI_PRIMARY_REPOSITORY_PATH/ios"
 pod install
-echo "=== Pods installed ==="
+
+echo "=== Done ==="
